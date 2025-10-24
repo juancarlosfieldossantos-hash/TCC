9c1ad614b581ae341264699e9cd82dda519a9c14
juancarlosfieldossantos-hash <juancarlosfieldossantos@gmail.com>
2025-10-23 21:06:09 -0300

    possivelmente o ultimo

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from core.aluno.aluno_controller import aluno_controller
-
+from core.usuario.usuario_controller import controller
 
 app = Flask(__name__)
 
 # registro das ontrollers
 app.register_blueprint(aluno_controller)
+app.register_blueprint(controller)
 
 
 if __name__ =="__main__":
--- core/Aluno/aluno_controller.py
+++ core/Aluno/aluno_controller.py
@@ -1,44 +1,61 @@
-from flask import Blueprint,request
+from flask import Blueprint, request, jsonify
 from core.aluno.aluno_service import AlunoService
 from core.aluno.aluno import Aluno
+from core.autenticacao.autenticacao import autenticacao
+from core.validador.validador import validar_dados
 
 aluno_service = AlunoService()
 
 aluno_controller = Blueprint('aluno', __name__, url_prefix='/alunos')
 
 @aluno_controller.route('/', methods=['GET'])
+@autenticacao
 def listar_alunos():
-    alunos = aluno_Service.listar_alunos()
+    alunos = aluno_service.listar_alunos()
     return jsonify(alunos)
 
 @aluno_controller.route('/', methods=['POST'])
+@autenticacao
 def adicionar_aluno():
     dados = request.get_json()
-    obj_aluno = Aluno(id=0, nome=dados["nome"],
-                      idade=dados["idade"], cpf=dados["cpf"])    
-    aluno_service.adicionar_aluno(obj_aluno)
-    return jsonify(aluno)
+
+    result = validar_dados(dados["cpf"], dados["nome"], dados["idade"])
+    if result != True:
+        return jsonify(result), 400
+
+    obj_aluno = Aluno(id=0, nome=dados["nome"], 
+                      idade=dados["idade"], cpf=dados["cpf"])
+    aluno = aluno_service.adicionar_aluno(obj_aluno)
+    return jsonify(aluno), 201
 
 @aluno_controller.route('/<int:id>', methods=['GET'])
+@autenticacao
 def obter_aluno(id):
-        aluno = AlunoService.obter_aluno_por_id(id)
-        if aluno:
-           return jsonify(aluno)
-        else:
-                return jsonify({"erro": "aluno não encontrado"}),404
-        
-@aluno_controller.route('/<int:id>', methods=['DELETE'])
+    aluno = aluno_service.obter_aluno_por_id(id)
+    if aluno:
+        return jsonify(aluno)
+    else:
+        return jsonify({"erro": "Aluno não encontrado"}), 404
+
+@aluno_controller.route('/<int:id>', methods=['DELETE'])   
+@autenticacao 
 def remover_aluno(id):
     sucesso = aluno_service.remover_aluno(id)
     return jsonify(sucesso)
 
-@aluno_controller.route('/',methods=['PUT'])
+@aluno_controller.route('/', methods=['PUT'])
+@autenticacao
 def atualizar_aluno():
-        dados = request.get_json()
-        obj_aluno = aluno(id=dados["id"],nome = dados["nome"],
-                          idade = dados["idade"], cpf=dados["cpf"])
-        aluno = alino_service.atualizar_aluno(obj_aluno)
-        if aluno:
-               return jsonify(aluno)
-        else:
-               return jsonify({"erro": "aluno não encontrado"}),404
+    dados = request.get_json()
+
+    result = validar_dados(dados["cpf"], dados["nome"], dados["idade"])
+    if result != True:
+        return jsonify(result), 400
+
+    obj_aluno = Aluno(id=dados["id"], nome=dados["nome"],
+                      idade=dados["idade"], cpf=dados["cpf"])
+    aluno = aluno_service.atualizar_aluno(obj_aluno)
+    if aluno:
+        return jsonify(aluno)
+    else:
+        return jsonify({"erro": "Aluno não encontrado"}), 404
